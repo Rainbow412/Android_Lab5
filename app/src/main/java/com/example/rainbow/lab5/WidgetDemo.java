3b9e9d3bc5104d09220fe4085afb8282fb89634a
Rainbow412 <annie0760@163.com>
2016-10-20 19:48:48 +0800

    完成1.0

---
+++ app/src/main/java/com/example/rainbow/lab5/WidgetDemo.java
@@ -0,0 +1,71 @@
+package com.example.rainbow.lab5;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.RemoteViews;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class WidgetDemo extends AppWidgetProvider {
+    private String STATICACTION = "Rainbow.example.lab5.staticreceiver";
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+        Intent clickInt = new Intent(context, MainActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(context, 0, clickInt, 0);
+
+        RemoteViews rv = new RemoteViews(context.getPackageName(), R.layout.widget_demo);
+
+        rv.setTextViewText(R.id.appwidget_text, "Widget");
+        rv.setImageViewResource(R.id.appwidget_image, R.mipmap.apple);
+
+        rv.setOnClickPendingIntent(R.id.appwidget_image, pi);
+
+        appWidgetManager.updateAppWidget(appWidgetIds, rv);
+
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        RemoteViews rv = new RemoteViews(context.getPackageName(), R.layout.widget_demo);
+        Bundle bundle = intent.getExtras();
+
+        if (intent.getAction().equals(STATICACTION)) {
+
+            String fruitName = bundle.getString("fruitName");
+            int img = bundle.getInt("img");
+            Log.d("WidgetDemo", fruitName);
+
+            rv.setTextViewText(R.id.appwidget_text, fruitName);
+            rv.setImageViewResource(R.id.appwidget_image, img);
+
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+            ComponentName thisAppWidget = new ComponentName(context.getPackageName(), WidgetDemo.class.getName());
+            int[] appWidgetIds = appWidgetManager.getAppWidgetIds(thisAppWidget);
+            appWidgetManager.updateAppWidget(appWidgetIds, rv);
+
+        }
+    }
+
+}
